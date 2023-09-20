bebd0db5d17cfcb488ce1194d352f13b371a0942
RJ_Infinity <rjinfinity2004@gmail.com>
2023-09-21 00:16:50 +0100

    added a testing suite

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
 Set-PSDebug -Trace 2
-gcc .\allocerTest.c .\RJAllocator.c -o allocerTest.exe -g -Wall -Wextra -pedantic -Wswitch-enum -Werror -std=c11
+gcc .\test.c .\RJAllocator.c -o test.exe -g -Wall -Wextra -pedantic -Wswitch-enum -Werror -std=c11
 Set-PSDebug -Off
